5cf2fc8ea8f792b6778bbfd30dc929fff04f4d43
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2017-09-26 09:47:28 +0300

    Create diode_tjhermometer_display_1.ino

---
+++ diode_tjhermometer_display_1.ino
@@ -0,0 +1,413 @@
+/*
+ base sketch: https://cdn.sparkfun.com/datasheets/Components/LED/_7Seg_Example.pde
+  4 digit 7 segment display: http://www.sparkfun.com/products/9483 
+ 7 segments + 4 digits + 1 colon = 12 pins required for full control 
+modified connexion by Nicu FLORICA (niq_ro) from http://nicuflorica.blogspot.com
+http://arduinotehniq.blogspot.com
+http://www.tehnic.go.ro
+http://www.arduinotehniq.com/
+ver.1.0 - thermometer with integer value
+*/
+
+int digit1 = 11; //PWM pin for control digit 1 (left side)
+int digit2 = 10; //PWM pin for control digit 2
+int digit3 = 9;  //PWM pin for control digit 3
+int digit4 = 6;  //PWM pin for control digit 4 (right side)
+
+int segA = 2; // pin for control "a" segment
+int segB = 3; // pin for control "b" segment
+int segC = 4; // pin for control "c" segment
+int segD = 5; // pin for control "d" segment
+int segE = 12; // pin for control e" segment
+int segF = 7; // pin for control "f" segment
+int segG = 8; // pin for control "g" segment
+int segDP = 13; // pin for control decimal point
+
+
+#define DIODA A5  // pin for conect 1n4148 diode and GND
+const int t0 = 20.3;
+const float vf0 = 573.44;
+int i;
+double dtemp, dtemp_avg, te;
+double voltageref = 1100;  // ideal value
+float corectie = 1.2;  // correction factor for reading when is use multiplexing method and other causes
+float t1;  
+int measurements = 10;
+
+int DISPLAY_BRIGHTNESS = 500;
+//Display brightness
+//Each digit is on for a certain amount of microseconds
+//Then it is off until we have reached a total of 20ms for the function call
+//Let's assume each digit is on for 1000us
+//Each digit is on for 1ms, there are 4 digits, so the display is off for 16ms.
+//That's a ratio of 1ms to 16ms or 6.25% on time (PWM).
+//Let's define a variable called brightness that varies from:
+//5000 blindingly bright (15.7mA current draw per digit)
+//2000 shockingly bright (11.4mA current draw per digit)
+//1000 pretty bright (5.9mA)
+//500 normal (3mA)
+//200 dim but readable (1.4mA)
+//50 dim but readable (0.56mA)
+//5 dim but readable (0.31mA)
+//1 dim but readable in dark (0.28mA)
+
+/*  
+//for common catode
+#define DIGIT_ON  HIGH
+#define DIGIT_OFF  LOW
+#define SEGMENT_ON  LOW
+#define SEGMENT_OFF HIGH
+*/
+
+// for common anode
+#define DIGIT_ON  LOW
+#define DIGIT_OFF  HIGH
+#define SEGMENT_ON  HIGH
+#define SEGMENT_OFF LOW
+
+
+byte bucla = 30;
+byte buclamax = 1;
+
+//int timp; 
+
+int maxbright = 1023; // 1023+7
+int lene = 0;
+int cicluri = 250; // if maxbright ~1000, cicluri = 125, if maxbright ~500 cicluri = 250;
+
+void setup() {
+  
+ analogReference(INTERNAL);  // http://tronixstuff.com/2013/12/12/arduino-tutorials-chapter-22-aref-pin/
+ pinMode(DIODA, INPUT_PULLUP);            // set the  pin IN with npull up to bias the diode
+
+  pinMode(segA, OUTPUT);
+  pinMode(segB, OUTPUT);
+  pinMode(segC, OUTPUT);
+  pinMode(segD, OUTPUT);
+  pinMode(segE, OUTPUT);
+  pinMode(segF, OUTPUT);
+  pinMode(segG, OUTPUT);
+  pinMode(segDP, OUTPUT);
+
+  pinMode(digit1, OUTPUT);
+  pinMode(digit2, OUTPUT);
+  pinMode(digit3, OUTPUT);
+  pinMode(digit4, OUTPUT);
+
+ Serial.begin(9600);
+ Serial.println("test for niq_ro");
+
+}
+
+void loop() {
+
+ dtemp_avg = 0;
+  for (i = 0; i < measurements; i++) {
+    float vf = analogRead(DIODA) * (voltageref / 1023.00);
+    //Serial.println(vf);
+    dtemp = (vf - vf0) * 0.4545454;
+    dtemp_avg = dtemp_avg + dtemp;
+ //  delay(1);
+  }
+  te = t0 - dtemp_avg / measurements;
+  te = te * corectie + 0.5;
+  if (te < 0) te = t1;
+  else
+  t1 = te;
+
+
+Serial.print("temp = ");
+Serial.println(te);
+
+
+for (i = 0; i < cicluri; i++) {
+displayNumber(te);
+}
+
+
+} // end main program
+
+
+//Given a number, turns on those segments
+//If number == 10, then turn off number
+void lightNumber(int numberToDisplay) {
+  switch (numberToDisplay){
+
+  case 0:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+  case 1:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+  case 2:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_OFF);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 3:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 4:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 5:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 6:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 7:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+  case 8:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 9:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  // all segment are ON
+  case 10:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_OFF);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+    // degree symbol made by niq_ro
+  case 11:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_OFF);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  // C letter made by niq_ro
+  case 12:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_OFF);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+  
+  // H letter made by niq_ro
+  case 13:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+ 
+  // L letter made by niq_ro
+  case 14:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_OFF);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+  // A letter made by niq_ro
+  case 15:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+  
+    // F letter made by niq_ro
+  case 16:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_OFF);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+    
+  // n letter made by niq_ro
+  case 17:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+    // o letter made by niq_ro
+  case 18:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+ 
+ // "Y" letter made by niq_ro
+  case 19:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  // "r" letter made by niq_ro
+  case 20:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_OFF);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  // "d" letter made by niq_ro
+  case 21:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+ }
+}
+
+
+// display temperature
+void displayNumber(int toDisplay) {
+  for(int digit = 4 ; digit > 0 ; digit--) {
+    //Turn on a digit for a short amount of time
+    switch(digit) {
+    case 1:
+      digitalWrite(digit1, DIGIT_ON);
+      lightNumber(toDisplay % 10);
+      toDisplay /= 10;
+      delayMicroseconds(DISPLAY_BRIGHTNESS); 
+      break;
+   case 2:
+      digitalWrite(digit2, DIGIT_ON);
+      lightNumber(toDisplay % 10);
+      toDisplay /= 10;
+      delayMicroseconds(DISPLAY_BRIGHTNESS); 
+      break;
+    case 3:
+      digitalWrite(digit3, DIGIT_ON);
+      lightNumber(11); // display degree symbol
+      delayMicroseconds(DISPLAY_BRIGHTNESS); 
+      break;
+    case 4:
+      digitalWrite(digit4, DIGIT_ON);
+      lightNumber(12); // display C letter
+      digitalWrite(segDP, SEGMENT_OFF);
+      delayMicroseconds(DISPLAY_BRIGHTNESS); 
+      break;
+    }
+     //Turn off all segments
+    lightNumber(10); 
+    digitalWrite(segDP, SEGMENT_OFF);
+     lene = maxbright - DISPLAY_BRIGHTNESS;
+    delayMicroseconds(lene); 
+
+    //Turn off all digits
+    digitalWrite(digit1, DIGIT_OFF);
+    digitalWrite(digit2, DIGIT_OFF);
+    digitalWrite(digit3, DIGIT_OFF);
+    digitalWrite(digit4, DIGIT_OFF);
+    digitalWrite(segDP, SEGMENT_OFF);
+}
+} 
